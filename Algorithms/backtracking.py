69a79bbc7ebf13241089edb722e487619ff6f61f
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2023-06-03 11:44:22 +0530

    3 June update

---
+++ Algorithms/backtracking.py
@@ -0,0 +1,13 @@
+# skeleton of a backtracking algorithm
+
+def function(parameters):
+    if condition:   # base component
+        print
+
+    else:           # recursive component
+        loop:
+            task
+            function(diffrent parameters)
+            undo task
+        
+
